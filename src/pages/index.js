99300dba7768d93f3958a0f516422232fd88cf8a
MyriamB on Nouvelle vague <myriam@myriadeweb.com>
2024-09-17 17:53:52 -0400

    implements ScrollTo Gsap plugin for internal anchor links

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,16 @@
-import React from 'react'
+import React, { useState, useRef } from 'react'
 import PageLayout from '../layouts/pageLayout'
-import { Link } from 'gatsby'
 import styled from 'styled-components'
-
 import { media } from '../styles/mixins.js'
+
+import { gsap } from "gsap";
+import { useGSAP } from '@gsap/react';
+import { ScrollToPlugin } from "gsap/ScrollToPlugin";
 
 import StatutsImmigrationList from '../components/statutsImmigrationList'
 import HistoiresList from '../components/histoiresList'
 
-const Section_1_hero = styled.section`
+const Section1Hero = styled.section`
   background-color: var(--color-bleu-tres-pale);
   background-image: url(/portrait4.webp);
   background-size: cover;
@@ -39,7 +41,7 @@ const Section_1_hero = styled.section`
   `};
 `;
 
-const Section_2_intro = styled.section`
+const Section2Intro = styled.section`
   .grid {
     gap: var(--v-spacer) 4rem;}
     
@@ -68,7 +70,7 @@ const Section_2_intro = styled.section`
     margin-top: var(--v-spacer);}
 `;
 
-const Section_3_statuts = styled.section`
+const Section3Statuts = styled.section`
   background: var(--color-bleu-tres-pale);
   h2 {
     text-transform: uppercase;
@@ -89,7 +91,7 @@ const Section_3_statuts = styled.section`
     line-height: 1.35;}
 `;
 
-const Section_4_video = styled.section`
+const Section5Video = styled.section`
   background: var(--color-bleu-clair);
   h2 {
     color: white;
@@ -97,64 +99,81 @@ const Section_4_video = styled.section`
   }
 `;
 
-const Section_6_partenaires = styled.section`
+const Section7Partenaires = styled.section`
   background: var(--color-bleu-tres-fonce);
   color: white;
 `;
 
+gsap.registerPlugin(useGSAP);
+gsap.registerPlugin(ScrollToPlugin);
+
 const IndexPage = () => {
+  const gsapContainerRef = useRef();
+  
+  const { contextSafe } = useGSAP({ scope: gsapContainerRef });
+  
+  const onClickHandler = contextSafe(() => {
+    gsap.to( window, { duration: 1.5, scrollTo: '#agir' });
+  });
+  
   return (
     <PageLayout>
-      <Section_1_hero >
-        <div className='overlay-text'>
-          <h1>
-            <span>Rêver</span> 
-            <span className='right'>à</span> 
-            <span>l'essentiel</span>
-          </h1>
-        </div>
-      </Section_1_hero>
-      
-      <Section_2_intro>
-        <div className='grid'>
-          <h2>Tout le monde a des rêves, mais ce n'est pas donné à tout le monde de pouvoir en faire une réalité.</h2>
-          <div>
-            <p>Les personnes im·migrantes sans statut ou à statut précaire sont souvent contraintes de mettre de côté leurs plus grandes aspirations pour ne rêver qu'à l'essentiel. En passant de grandes ambitions aux désirs les plus simples—et les plus humains—la campagne Rêver à l'essentiel met en lumière les obstacles auxquels ces personnes doivent faire face pour vivre dignement lorsque leur quotidien est dicté par leur statut migratoire.</p>
-            <Link to='#agir' className='button'>Soutenir la cause</Link>
+      <div ref={gsapContainerRef}  style={{overflow: 'scroll'}}>
+        <Section1Hero >
+          <div className='overlay-text'>
+            <h1>
+              <span>Rêver</span> 
+              <span className='right'>à</span> 
+              <span>l'essentiel</span>
+            </h1>
           </div>
-        </div>
-      </Section_2_intro>
-      
-      <Section_3_statuts>
-        <div className='grid'>
-          <h2>Les statuts d'immigration</h2>
-          <p>Connaître la réalité des personnes im·migrantes au statut d'immigration absent ou précaire nécessite une meilleure compréhension des termes liés à l'enjeu. Apprenez-en davantage sur les différentes situations qui affectent les personnes im·migrantes.</p>
-        </div>
-        <StatutsImmigrationList />
-      </Section_3_statuts>
-      
-      <HistoiresList />
-      
-      <Section_4_video id='video'>
-        <h2>De toi à moi: témoignage d'une personne sans statut d'immigration.</h2>
-        <p>... Vidéo à venir ...</p>
-      </Section_4_video>
-      
-      <section id='agir'>
-        <h2>Comment vous pouvez aider</h2>
-        ...
-      </section>
-      
-      <Section_6_partenaires id='plus-loin'>
-        <h2>Pour aller plus loin</h2>
-        ...
-      </Section_6_partenaires>
-      
-      <section id='a-propos'>
-        <h2>À propos</h2>
-        ...
-      </section>
-      
+        </Section1Hero>
+        
+        <Section2Intro>
+          <div className='grid'>
+            <h2>Tout le monde a des rêves, mais ce n'est pas donné à tout le monde de pouvoir en faire une réalité.</h2>
+            <div>
+              <p>Les personnes im·migrantes sans statut ou à statut précaire sont souvent contraintes de mettre de côté leurs plus grandes aspirations pour ne rêver qu'à l'essentiel. En passant de grandes ambitions aux désirs les plus simples—et les plus humains—la campagne Rêver à l'essentiel met en lumière les obstacles auxquels ces personnes doivent faire face pour vivre dignement lorsque leur quotidien est dicté par leur statut migratoire.</p>
+              <button 
+                className='button soutenir'
+                onClick={onClickHandler}
+              >
+                Soutenir la cause
+              </button>
+            </div>
+          </div>
+        </Section2Intro>
+        
+        <Section3Statuts>
+          <div className='grid'>
+            <h2>Les statuts d'immigration</h2>
+            <p>Connaître la réalité des personnes im·migrantes au statut d'immigration absent ou précaire nécessite une meilleure compréhension des termes liés à l'enjeu. Apprenez-en davantage sur les différentes situations qui affectent les personnes im·migrantes.</p>
+          </div>
+          <StatutsImmigrationList />
+        </Section3Statuts>
+        
+        <HistoiresList />
+        
+        <Section5Video id='video'>
+          <h2>De toi à moi: témoignage d'une personne sans statut d'immigration.</h2>
+          <p>... Vidéo à venir ...</p>
+        </Section5Video>
+        
+        <section id='agir'>
+          <h2>Comment vous pouvez aider</h2>
+          ...
+        </section>
+        
+        <Section7Partenaires id='plus-loin'>
+          <h2>Pour aller plus loin</h2>
+          ...
+        </Section7Partenaires>
+        
+        <section id='a-propos'>
+          <h2>À propos</h2>
+          ...
+        </section>
+      </div>
     </PageLayout>
   )
 }
