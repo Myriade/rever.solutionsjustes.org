a4140c8cfa0f69dff758cb996efe0958cddf3aab
MyriamB on Nouvelle vague <myriam@myriadeweb.com>
2024-10-09 10:10:45 -0400

    OG image tag close fix

--- src/pages/connaitre.js
+++ src/pages/connaitre.js
@@ -1031,7 +1031,7 @@ export const Head = () => (
     <meta property='og:title' content='Connaître l’essentiel | Solutions justes' />
     <meta property='og:description' content='Connaître l’essentiel de certains statuts d’immigration précaires et de l’absence de statut. Récits, mythes et réalités. Bonjour, je suis Maria et je suis une personne sans statut d’immigration.' />
     <meta property='og:image' content='https://rever.solutionsjustes.org/rever-essentiel-solutions-justes-mcm-og.jpg' />
-    <meta property='og:image:alt' content='Je rêvais d’une meilleure vie au Québec. Je rêve maintenant de ne plus vivre dans l’angoisse de devoir quitter. - Personne en attente de sa résidence permanente depuis 7 ans. Informez-vous sur les réalités migratoires au Québec. Solutions Justes, MCM'>
+    <meta property='og:image:alt' content='Je rêvais d’une meilleure vie au Québec. Je rêve maintenant de ne plus vivre dans l’angoisse de devoir quitter. - Personne en attente de sa résidence permanente depuis 7 ans. Informez-vous sur les réalités migratoires au Québec. Solutions Justes, MCM' />
     <meta property='og:url' content='https://rever.solutionsjustes.org/connaitre' />
     <meta property='og:type' content='website' />
   </>
--- src/pages/index.js
+++ src/pages/index.js
@@ -476,7 +476,7 @@ export const Head = () => (
     <meta property='og:title' content='Rêver à l’essentiel | Solutions justes' />
     <meta property='og:description' content='Chaque rêve mérite de ne pas être essentiel ! Ensemble, apprenons la réalité de l’immigration humanitaire et faisons entendre les histoires de ceux et celles qui aspirent à un avenir meilleur.' />
     <meta property='og:image' content='https://rever.solutionsjustes.org/rever-essentiel-solutions-justes-mcm-og.jpg' />
-    <meta property='og:image:alt' content='Je rêvais d’une meilleure vie au Québec. Je rêve maintenant de ne plus vivre dans l’angoisse de devoir quitter. - Personne en attente de sa résidence permanente depuis 7 ans. Informez-vous sur les réalités migratoires au Québec. Solutions Justes, MCM'>
+    <meta property='og:image:alt' content='Je rêvais d’une meilleure vie au Québec. Je rêve maintenant de ne plus vivre dans l’angoisse de devoir quitter. - Personne en attente de sa résidence permanente depuis 7 ans. Informez-vous sur les réalités migratoires au Québec. Solutions Justes, MCM' />
     <meta property='og:url' content='https://rever.solutionsjustes.org/' />
     <meta property='og:type' content='website' />
   </>
