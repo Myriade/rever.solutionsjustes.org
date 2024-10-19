c819eea88c7b79ec15b56861254671ddbaa3a733
MyriamB on Nouvelle vague <myriam@myriadeweb.com>
2024-10-19 18:15:52 -0400

    Cookie consent refactor : only show the consent banner if necessary

---
+++ src/components/cookieConsent.js
@@ -0,0 +1,94 @@
+import React, { useState, useEffect } from 'react'
+import styled from 'styled-components'
+import { media } from '../styles/mixins.js'
+// import { gsap } from 'gsap'
+// import { useGSAP } from '@gsap/react'
+
+const Consentement = styled.section`
+	position: fixed;
+	display: grid;
+	gap: 1rem;
+	align-items: center;
+	z-index: 300;
+	bottom: 0;
+	right: 10vw;
+	left: 10vw;
+	background: var(--color-bleu-tres-fonce);
+	color: white;
+	padding: 0.5rem var(--h-spacer);
+	p, .button {
+		font-size: 0.9rem;}
+	.button {
+		display: block;
+		background-color: white;
+		color: var(--color-bleu-tres-fonce);
+		float: right;
+		margin-right: 5em;
+		&:after, &:before {
+			background-color: white;}}
+			
+	a {
+		text-decoration: underline;
+		font-weight: normal;
+		&:hover {
+			text-decoration-thickness: 2px;
+		}
+	}
+			
+	${media.mediumUp`
+		grid-template-columns: auto auto;
+		.button {
+			float: none;
+			margin-right: 2em;
+		}
+	`}
+	
+`;
+
+export default function CookieConsent() {
+	const [isConsentOk, setIsConsentOk] = useState(true);
+	
+	// event handler
+	const onConsentClick = () => {
+		setIsConsentOk(true);
+		localStorage.setItem('cookiesConsent', 'true');
+		console.log('Cookie consent clicked');
+	}
+	
+	// The isConsentOk state is assumed to be true up until the local storage is set to false
+	useEffect( () => {
+		// verify if there is a local consent already, and store its value in the localConsent state
+		const localConsentValue = localStorage.getItem('cookiesConsent');
+		
+		// function to set consent false in local storage and state
+		const setConsentFalse = () => {
+			localStorage.setItem('cookiesConsent', 'false');
+			setIsConsentOk(false);
+		}
+		
+		// Logic
+		if ( !localConsentValue || localConsentValue === 'false' ) {
+			// if there is no local value or the local value is false (string), 
+			// wait a few seconds and call setConsentFalse function
+			setTimeout( setConsentFalse, 3500 );
+		}
+		
+	}, [isConsentOk]);
+	
+	if (!isConsentOk) {
+		return (
+			<Consentement>
+				<p>Nous utilisons des cookies pour comprendre comment vous interagissez avec notre site. En poursuivant la visite de ce site web, vous consentez à notre utilisation de ces cookies. <a href='https://www.solutionsjustes.org/politique-confidentialite' target='_blank' rel='noreferrer'>Voir notre politique de confidentialité</a></p>
+				<div>
+					<button
+						className='button'
+						aria-label='fermer l\u2019avis'
+						onClick={onConsentClick}
+					>J'ai compris</button>
+				</div>
+			</Consentement>
+		);
+	} else {
+		return null ;
+	}
+}
--- src/layouts/pageLayout.js
+++ src/layouts/pageLayout.js
@@ -1,13 +1,14 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import { Link } from 'gatsby'
 import { StaticImage } from 'gatsby-plugin-image'
 import styled from 'styled-components'
-
 import { media } from '../styles/mixins.js'
-
 import '../styles/globals.scss'
 
-import TempProtectLayout from './TempProtectLayout.js';
+import CookieConsent from '../components/cookieConsent'
+
+import { gsap } from 'gsap'
+import { useGSAP } from '@gsap/react'
 
 const Header = styled.header`
   position: fixed;
@@ -95,65 +96,7 @@ const Footer = styled.footer`
     font-size: 0.8rem;}
 `;
 
-const Consentement = styled.section`
-  position: fixed;
-  display: grid;
-  gap: 1rem;
-  align-items: center;
-  z-index: 300;
-  bottom: 0;
-  right: 10vw;
-  left: 10vw;
-  background: var(--color-bleu-tres-fonce);
-  color: white;
-  padding: 0.5rem var(--h-spacer);
-  p, .button {
-    font-size: 0.9rem;}
-  .button {
-    display: block;
-    background-color: white;
-    color: var(--color-bleu-tres-fonce);
-    float: right;
-    margin-right: 5em;
-    &:after, &:before {
-      background-color: white;}}
-      
-  a {
-    text-decoration: underline;
-    font-weight: normal;
-    &:hover {
-      text-decoration-thickness: 2px;
-    }
-  }
-      
-  ${media.mediumUp`
-    grid-template-columns: auto auto;
-    .button {
-      float: none;
-      margin-right: 2em;
-    }
-  `}
-  
-`;
-
 export default function PageLayout({ children }) {
-  const [isConsentOk, setIsConsentOk] = useState(null);
-  
-  useEffect( () => {
-    let localConsent = localStorage.getItem('cookiesConsent');
-    
-    if ( !localConsent && !isConsentOk ) {
-      // if there is no local value stored and the state is false, set the local value to 'false' (string)
-      localStorage.setItem('cookiesConsent', 'false'); 
-    } else if ( localConsent === 'false' && isConsentOk ) { 
-      // if the local stored value is 'false' but the state is true, set the local value to 'true'
-      localStorage.setItem('cookiesConsent', 'true');
-    } else if ( localConsent === 'true' ) {
-      // if the local stored value is 'true', set the state to true (user had already visited the site)
-      setIsConsentOk(true);
-    }
-  }, [isConsentOk]);
-  
   return (
     <>
       <Header>
@@ -187,19 +130,8 @@ export default function PageLayout({ children }) {
         <p>Copyright © 2024 <a href='https://www.solutionsjustes.org/'>Solutions Justes</a></p>
       </Footer>
       
-      { !isConsentOk ? 
-        <Consentement>
-          <p>Nous utilisons des cookies pour comprendre comment vous interagissez avec notre site. En poursuivant la visite de ce site web, vous consentez à notre utilisation de ces cookies. <a href='https://www.solutionsjustes.org/politique-confidentialite' target='_blank' rel='noreferrer'>Voir notre politique de confidentialité</a></p>
-          <div>
-            <button
-              className='button'
-              aria-label='fermer l\u2019avis'
-              onClick={ () => { setIsConsentOk(true) } }
-            >J'ai compris</button>
-          </div>
-        </Consentement>
-        : ''
-      }
+      <CookieConsent />
+      
     </>
   )
 }
