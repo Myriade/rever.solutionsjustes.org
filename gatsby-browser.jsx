cc3f9f6a502b13e81106bce7f004a6746e5873e0
MyriamB on Nouvelle vague <myriam@myriadeweb.com>
2024-11-05 21:37:33 -0500

    gatsby browser config test fix

--- gatsby-browser.jsx
+++ gatsby-browser.jsx
@@ -1,11 +1,21 @@
 const React = require("react");
 
 
+// Logs when the client route changes
+exports.onRouteUpdate = ({ location, prevLocation }) => {
+	console.log("new pathname", location.pathname)
+	console.log("old pathname", prevLocation ? prevLocation.pathname : null)
+}
+
 exports.shouldUpdateScroll = () => {
 	if (!window.location.hash) {
-		return false;
+		// if no hash is present, force the page to load at the very top
+		console.log('browser no location hash');
+		window.scrollTo(0, 0);
 	} else if (window.location.hash) {
+		// if url has a hash, do nothing
 		const hashSubstring = window.location.hash.substring(1);
-		return false;
+		console.log('browser hashSubstring = ', hashSubstring);
 	}
+	return false;
 }
