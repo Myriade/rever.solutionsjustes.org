50973c3f21014fd9eebcb2e4d9aa33748927d0b2
MyriamB on Nouvelle vague <myriam@myriadeweb.com>
2024-11-12 15:06:29 -0500

    Quiz Data temporaire (errata précédent commit)

--- src/data/quizData.js
+++ src/data/quizData.js
@@ -26,51 +26,51 @@ export default function quizData() { return [
 		],
 		explications: [`Apparaît ici une phrase ou deux d'explications (à rédiger)`]
 	} , 
-	// {
-	// 	id: 'enceinte',
-	// 	title: `Femme enceinte`,
-	// 	situation: `<p>Une femme se présente à l'hôpital car elle est à 41 semaine de grossesse et doit se faire "déclencher", tel que l'indique le médecin qui a fait son suivi de grossesse dans une clinique privée. Comme il faut planifier la césarienne, ce soin n'est pas consideré comme une "urgence" aux yeux de l'hôpital et l'équipe refuse de planifier ce soin si la femme ne peut pas payer d'avance.</p>
-	// 	<p>En plus du stress de savoir si elle et son bébé vont pouvoir se rendre à terme sain et sauf, elle subit des violences verbales de la part du personnel hospitalier (par exemple : "no money, no baby").</p>
-	// 	<p>Après avoir fait plusieurs hôpitaux et passer des heures et des heures aux urgences et dans les services administratifs à expliquer sa situation, muni de son dossier médical, elle trouve enfin un médecin qui accepte de programmer sa césarienne et de prendre une entente de paiement avec elle.</p>
-	// 	<p>Comme les personnes im·migrantes sans couverture sont facturées à 200% du prix, elle sera facturée 18 000$, facture qu'elle mettra 10 ans à rembourser (sur une base de 200$ par mois).</p>`,
-	// 	choix: [
-	// 		{
-	// 			text: 'Sans statut d’immigration',
-	// 			isRightAnswer: true
-	// 		},
-	// 		{
-	// 			text: 'Étudiant·e international·e',
-	// 			isRightAnswer: false
-	// 		},
-	// 		{
-	// 			text: 'Liée à un permis de travail fermé',
-	// 			isRightAnswer: false
-	// 		}
-	// 	],
-	// 	explications: [`Apparaît ici une phrase ou deux d'explications (à rédiger) `]
-	// } , 
-	// {
-	// 	id: 'violenceconjugale',
-	// 	title: 'Violence conjugale',
-	// 	situation: `<p>Une personne im·migrante est arrivée au Québec pour rejoindre son conjoint canadien dans le cadre d'un processus de parrainage, dans l'espoir de pouvoir vivre ensemble.</p>
-	// 	<p>Après quelques mois de vie commune, le conjoint soustrait à sa partenaires ses papiers d’identité, devient violent psychologiquement et physiquement. Il la menace de retirer la demande de parrainage et de la faire déporter, si elle le dénonçait à la police.</p>
-	// 	<p>Malgré la crainte de perdre son statut d’immigration, elle décide de quitter son conjoint, afin de fuir ces violences et reprendre du pouvoir sans sa vie. Elle se retrouve alors en situation d'itinérance, sans savoir si la demande de parrainage a été annulée.</p>`,
-	// 	choix: [
-	// 		{
-	// 			text: 'En permis de séjour temporaire',
-	// 			isRightAnswer: true
-	// 		},
-	// 		{
-	// 			text: 'Étudiant·e international·e',
-	// 			isRightAnswer: false
-	// 		},
-	// 		{
-	// 			text: 'Liée à un permis de travail fermé',
-	// 			isRightAnswer: false
-	// 		}
-	// 	],
-	// 	explications: [`Apparaît ici une phrase ou deux d'explications (à rédiger) `]
-	// } , 
+	{
+		id: 'enceinte',
+		title: `Femme enceinte`,
+		situation: `<p>Une femme se présente à l'hôpital car elle est à 41 semaine de grossesse et doit se faire "déclencher", tel que l'indique le médecin qui a fait son suivi de grossesse dans une clinique privée. Comme il faut planifier la césarienne, ce soin n'est pas consideré comme une "urgence" aux yeux de l'hôpital et l'équipe refuse de planifier ce soin si la femme ne peut pas payer d'avance.</p>
+		<p>En plus du stress de savoir si elle et son bébé vont pouvoir se rendre à terme sain et sauf, elle subit des violences verbales de la part du personnel hospitalier (par exemple : "no money, no baby").</p>
+		<p>Après avoir fait plusieurs hôpitaux et passer des heures et des heures aux urgences et dans les services administratifs à expliquer sa situation, muni de son dossier médical, elle trouve enfin un médecin qui accepte de programmer sa césarienne et de prendre une entente de paiement avec elle.</p>
+		<p>Comme les personnes im·migrantes sans couverture sont facturées à 200% du prix, elle sera facturée 18 000$, facture qu'elle mettra 10 ans à rembourser (sur une base de 200$ par mois).</p>`,
+		choix: [
+			{
+				text: 'Sans statut d’immigration',
+				isRightAnswer: true
+			},
+			{
+				text: 'Étudiant·e international·e',
+				isRightAnswer: false
+			},
+			{
+				text: 'Liée à un permis de travail fermé',
+				isRightAnswer: false
+			}
+		],
+		explications: [`Apparaît ici une phrase ou deux d'explications (à rédiger) `]
+	} , 
+	{
+		id: 'violenceconjugale',
+		title: 'Violence conjugale',
+		situation: `<p>Une personne im·migrante est arrivée au Québec pour rejoindre son conjoint canadien dans le cadre d'un processus de parrainage, dans l'espoir de pouvoir vivre ensemble.</p>
+		<p>Après quelques mois de vie commune, le conjoint soustrait à sa partenaires ses papiers d’identité, devient violent psychologiquement et physiquement. Il la menace de retirer la demande de parrainage et de la faire déporter, si elle le dénonçait à la police.</p>
+		<p>Malgré la crainte de perdre son statut d’immigration, elle décide de quitter son conjoint, afin de fuir ces violences et reprendre du pouvoir sans sa vie. Elle se retrouve alors en situation d'itinérance, sans savoir si la demande de parrainage a été annulée.</p>`,
+		choix: [
+			{
+				text: 'En permis de séjour temporaire',
+				isRightAnswer: true
+			},
+			{
+				text: 'Étudiant·e international·e',
+				isRightAnswer: false
+			},
+			{
+				text: 'Liée à un permis de travail fermé',
+				isRightAnswer: false
+			}
+		],
+		explications: [`Apparaît ici une phrase ou deux d'explications (à rédiger) `]
+	} , 
 	{
 		id: 'lgbtq',
 		title: 'LGBTQ+',
