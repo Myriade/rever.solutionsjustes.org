39f104279081741200e1767015aa9f992c2e63dc
MyriamB on Nouvelle vague <myriam@myriadeweb.com>
2025-09-18 14:21:04 -0400

    Adds interactive SVG animation component
    Implements a new component showcasing interactive SVG animations
    using GSAP for drawing paths on scroll and click events.
    
    It allows users to trigger animations on specific paths,
    reset them, and observe scroll-triggered animations.

--- src/components/pAnimation.js
+++ src/components/pAnimation.js
@@ -12,6 +12,10 @@ import LiereTest from '../images/enracinee/LiereTest.js'
 const Styled = styled.section`
 	background: white;
 	
+	path {
+		stroke-dashoffset: 5%
+	}
+	
 	${media.mediumUp`
 		
 	`}
@@ -23,32 +27,81 @@ gsap.registerPlugin(useGSAP, ScrollTrigger, DrawSVGPlugin);
 export default function PAnimation() {
 	
 	const gsapScopeRef = useRef();
-	
 	const { contextSafe } = useGSAP({ scope: gsapScopeRef });
+	useGSAP(() => {
+		gsap.set("path", {
+			drawSVG: "0%"
+		});
+	},{ scope: gsapScopeRef }); 
 	
-	const onClickEvent = contextSafe(() => {
-		gsap.from("path", {
-			duration:3,
-			drawSVG: 0}
-		);
+	const onClickEvent = contextSafe((pathName) => {
+		if (pathName === 'epines') {
+			gsap.to('#click .epine path', {
+				duration: 3,
+				drawSVG: "100%"}
+			);
+		} else if (pathName === 'reset') {
+			gsap.set("#click path", {
+				drawSVG: "0%"
+			});
+		} else {
+			gsap.to(`#click #${pathName} path`, {
+				duration: 3,
+				drawSVG: "100%"}
+			);
+		}
 	});
 	
+	useGSAP(() => {
+		gsap.to("#scroll path", {
+			drawSVG: "100%", // End with path fully drawn
+			ease: "none", // Linear progression with scroll
+			scrollTrigger: {
+				trigger: "#scroll", // Use body as trigger
+				start: "top 80%", // Start when top of body hits top of viewport
+				end: "bottom 75%", // End when bottom of body hits bottom of viewport
+				scrub: 1, // Smooth scrubbing, takes 1 second to "catch up"
+				markers: false
+			}
+		})
+	},{ scope: gsapScopeRef }); 
 	
 	return (
 		<div ref={gsapScopeRef} id='gsap-container'>
 			<Styled>
-				<h2>Page juste pour tester une animation vectorielle</h2>
-				<h3>Défiler vers le bas 👇 👇</h3>
-				<hr/>
-				<button onClick={onClickEvent}>Click-moi</button>
-				<p className='box'>Ça va bouger</p>
-				<hr/>
+				<h2>Tests d'animations vectorielles</h2>
 				
-				<section
-					style={{width: '500px', height: '500px'}}
-				>
+				<hr/>
+				<h3>Déployer les lignes une à la fois</h3>
+				<p>Cliquez les boutons pour voir chacune des lignes se déployer en 3 secondes</p>
+				
+				<div style={{display: 'flex', gap: '1rem'}}>
+					<button onClick={() => onClickEvent('base_x5F_1')}>base_x5F_1</button> 
+					<button onClick={() => onClickEvent('base_x5F_2')}>base_x5F_2</button> 
+					<button onClick={() => onClickEvent('base_x5F_3')}>base_x5F_3</button> 
+					<button onClick={() => onClickEvent('ombre_x5F_2')}>ombre_x5F_2</button> 
+					<button onClick={() => onClickEvent('ombre_x5F_3')}>ombre_x5F_3</button> 
+					<button onClick={() => onClickEvent('ombre_x5F_1_x5F_partie_x5F_1')}>ombre_x5F_1_x5F_partie_x5F_1</button>
+					<button onClick={() => onClickEvent('ombre_x5F_1_x5F_partie_x5F_2')}>ombre_x5F_1_x5F_partie_x5F_2</button>
+					<button onClick={() => onClickEvent('epines')}>Épines</button>
+					<button onClick={() => onClickEvent('reset')}>Réinitialiser</button>
+				</div>
+				
+				<section style={{width: '500px', height: '500px'}} id='click' >
 					<LiereTest />
 				</section>
+				
+				<hr/>
+				<h3>Déployer toutes les lignes au scroll, même rythme</h3>
+				<p>Défiler vers le bas 👇 👇</p>
+				
+				<section style={{width: '500px', height: '500px'}} id="scroll">
+					<LiereTest />
+				</section>
+				
+				<hr/>
+				<h3>Déployer toutes les lignes au scroll, rythmes différents</h3>
+				<p style={{minHeight: '30vh'}}>[À venir]</p>
 				
 			</Styled>
 		</div>
