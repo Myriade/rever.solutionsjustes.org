6e03eee8b510ada64fcb42fc9323c65e68135992
MyriamB on Nouvelle vague <myriam@myriadeweb.com>
2025-09-22 10:15:42 -0400

    Enracinée : Adds initial structure for page content
    Implements the basic layout for the page content,
    including a banner, video section, and chapter sections
    with images and text. Styles the components for
    visual presentation and incorporates localized text.

--- src/components/pEnracinee.js
+++ src/components/pEnracinee.js
@@ -4,10 +4,42 @@ import { media } from '../styles/mixins.js'
 import { StaticImage } from "gatsby-plugin-image"
 import { Link } from 'gatsby'
 
-const Section1Hero = styled.div`
+const Banniere = styled.div`
+	background: white;
 	width: 100%;
 	padding: initial !important;
 	display: grid;
+	
+	.temp {
+		padding: 5vh}
+	
+	${media.mediumUp``};
+`;
+
+const Video = styled.section`
+	iframe {
+		width: 100%;
+		height: 30vh;
+	}
+	${media.mediumUp``};
+`;
+
+const Chapitre = styled.section`
+
+	img {
+		border: 1rem solid #3a1737;}
+
+	.text {
+		background: #b5bca2;
+		padding: 2rem;
+		border-radius: 3rem}
+		
+	.color2 .text {
+		background: #729b76;}
+	.color3 .text {
+		background: #295534;}
+	.color4 .text {
+		background: #2d3837;}
 	
 	${media.mediumUp``};
 `;
@@ -25,24 +57,81 @@ const PEnracinee = ({lang, texts}) => {
 	
 	return (
 		<div ref={gsapContainerRef} id='gsap-container'>
-			<Section1Hero>
-				<h1>Bannière</h1>
-				<p>image, titre, bouton sortie rapide</p>
-			</Section1Hero>
+			<Banniere>
+				<div className='temp'>
+					<h1>Bannière</h1>
+					<p>image, titre, bouton sortie rapide</p>
+				</div>
+			</Banniere>
 			<hr/>
-			<div>
-				<h2>Vidéo</h2>
-			</div>
-			<hr/>
-			<div>
-				<h2>{texts.bloc1.titre}</h2>
-				{texts.bloc1.texte.map((item, index) => <p key={index}>{item}</p>)}
-			</div>
-			<hr/>
-			<div>
-				<h2>{texts.bloc2.titre}</h2>
-				{texts.bloc2.texte.map((item, index) => <p key={index}>{item}</p>)}
-			</div>
+			
+			<Video>
+				<iframe src="https://player.vimeo.com/video/235215203?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media; web-share" referrerpolicy="strict-origin-when-cross-origin"></iframe>
+				<script src="https://player.vimeo.com/api/player.js"></script>
+			</Video>
+			
+			<Chapitre><div id='chapitre1' className=''>
+				<StaticImage 
+					src='../images/MCM_Visuel-Histoire.png'
+					alt='temp'
+				/>
+				<div className='text'>
+					<h2>{texts.bloc1.titre}</h2>
+					{texts.bloc1.texte.map((item, index) => <p key={index}>{item}</p>)}
+				</div>
+			</div></Chapitre>
+				
+			<Chapitre><div id='chapitre2' className='color2'>
+				<StaticImage 
+					src='../images/MCM_Visuel-Histoire.png'
+					alt='temp'
+				/>
+				<div className='text'>
+					<h2>{texts.bloc2.titre}</h2>
+					{texts.bloc2.texte.map((item, index) => <p key={index}>{item}</p>)}
+				</div>
+			</div></Chapitre>
+			
+			<Chapitre><div id='chapitre3' className='color3'>
+				<StaticImage 
+					src='../images/MCM_Visuel-Histoire.png'
+					alt='temp'
+				/>
+				<div className='text'>
+					<h2>Bloc 3</h2>
+				</div>
+			</div></Chapitre>
+			
+			<Chapitre><div id='chapitre4' className='color4'>
+				<StaticImage 
+					src='../images/MCM_Visuel-Histoire.png'
+					alt='temp'
+				/>
+				<div className='text'>
+					<h2>Bloc 4</h2>
+				</div>
+			</div></Chapitre>
+			
+			<Chapitre><div id='chapitre5' className='color3'>
+				<StaticImage 
+					src='../images/MCM_Visuel-Histoire.png'
+					alt='temp'
+				/>
+				<div className='text'>
+					<h2>Bloc 5</h2>
+				</div>
+			</div></Chapitre>
+			
+			<Chapitre><div id='chapitre6' className='color2'>
+				<StaticImage 
+					src='../images/MCM_Visuel-Histoire.png'
+					alt='temp'
+				/>
+				<div className='text'>
+					<h2>Bloc 6</h2>
+				</div>
+			</div></Chapitre>
+		
 		</div>
 	)
 }
