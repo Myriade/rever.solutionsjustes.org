273f78ffe0ac5e0cfa190d8a582c05f97dfc34c2
MyriamB on Nouvelle vague <myriam@myriadeweb.com>
2025-09-26 18:36:10 -0400

    Enracinee : premier prototype de la mise en page

--- src/components/pEnracinee.js
+++ src/components/pEnracinee.js
@@ -13,48 +13,146 @@ const Banniere = styled.div`
 	.temp {
 		padding: 5vh}
 	
-	${media.mediumUp``};
+	${media.desktopUp``};
 `;
 
-const Video = styled.section`
+const Intro = styled.section`
+	background: white;
+	display: flex;
+	gap: var(--v-spacer) 0.5rem;
+	flex-wrap: wrap;
+	align-items: flex-start;
+	padding-bottom: 0 !important;
+	margin-bottom: 10vh;
+	
+	.chapitre {
+		margin-top: 10vh;
+		max-width: 85%}
+	
+	${media.desktopUp`
+		> div:nth-child(2) {
+			max-width: 30vw !important;
+			margin-left: calc(var(--h-spacer) * 2);
+			.gatsby-image-wrapper {
+				right: -5rem;}
+		.chapitre {
+			max-width: 100%;}}
+	`};
+`;
+
+const Video = styled.div`
+	width: 100%;
+	display: grid;
+	grid-template-columns: 0 auto;
+		
 	iframe {
 		width: 100%;
-		height: 30vh;
-	}
-	${media.mediumUp``};
+		height: 100%;}
+		
+	${media.mediumUp`
+		.set-height {
+			height: clamp(500px, 40vh, 800px);
+			width: 0;}
+	`}
+		
+	${media.desktopUp`
+		width: 50vw;
+			
+		iframe {
+			min-width: 50vw;}
+	`};
 `;
 
 const Chapitres = styled.section`
-	display: grid;
-	gap: 5vw;
-	
-	section:nth-child(even) {
-		padding-left: 10vw}
-	section:nth-child(odd) {
-		padding-right: 10vw}
+	display: flex;
+	flex-wrap: wrap;
+	gap: 16vh 6%;
+	margin-inline: 2rem;
 `;
 
-const Chapitre = styled.section`
-	img {
-		border: 1rem solid #3a1737;}
-
-	.text {
+const Chapitre = styled.div`
+	max-width: 100% !important;
+	.chapitre {
 		background: #b5bca2;
 		padding: 2rem;
-		border-radius: 3rem}
+		border-radius: 3rem;}
 		
-	.color2 .text {
+	&:nth-child(odd) .chapitre {
+		margin-right: calc(var(--h-spacer) * 2);
+		.gatsby-image-wrapper {
+			left: -15vw;}}
+
+	.text {
+		p {
+			display: none;}
+		p:nth-of-type(1) {
+			display: block;
+			font-weight: bold;}
+		p:nth-of-type(2) {
+			display: block;}
+		p:nth-of-type(3) {
+			display: block;
+			white-space: nowrap;
+			max-width: 70ch;
+			overflow: hidden;
+			text-overflow: "... [défilement vers la suite, à coder]";}}
+		
+	h2 {
+		font-size: 1.5rem;
+		letter-spacing: 0.05em;
+		margin-bottom: 0.25rem;
+		margin-top: -1rem;}
+		
+	.chapitre.color2 {
 		background: #729b76;}
-	.color3 .text {
+		
+	.chapitre.color3 {
 		background: #295534;
 		h2, p {
 			color: white;}}
-	.color4 .text {
+			
+	.chapitre.color4 {
 		background: #2d3837;
 		h2, p {
 			color: white;}}
 	
-	${media.mediumUp``};
+	.gatsby-image-wrapper {
+		width: 120%;
+		position: relative;
+		top: -10vh;
+		border: 1rem solid #3a1737;
+		--size: 0.75rem;
+		--g: #0000 98%, #000;
+		mask: 
+			radial-gradient(var(--size) at 0 0, var(--g)) 0 0,
+			radial-gradient(var(--size) at 100% 0, var(--g)) 100% 0,
+			radial-gradient(var(--size) at 0 100%, var(--g)) 0 100%,
+			radial-gradient(var(--size) at 100% 100%, var(--g)) 100% 100%;
+		mask-size: 51% 51%;
+		mask-repeat: no-repeat;
+		-webkit-mask: 
+			radial-gradient(var(--size) at 0 0, var(--g)) 0 0,
+			radial-gradient(var(--size) at 100% 0, var(--g)) 100% 0,
+			radial-gradient(var(--size) at 0 100%, var(--g)) 0 100%,
+			radial-gradient(var(--size) at 100% 100%, var(--g)) 100% 100%;
+		-webkit-mask-size: 51% 51%;
+		-webkit-mask-repeat: no-repeat;}
+	
+	${media.desktopUp`
+		width: 47%;
+		
+		.chapitre {
+			margin-inline: 0 !important;
+			.gatsby-image-wrapper {
+				left: -5vw !important;}}
+				
+		.gatsby-image-wrapper {
+			width: auto;
+			max-width: 70vw;}
+			
+		#chapitre3, #chapitre5 {
+			margin-top: 20vh;}
+	`};
 `;
 
 const localisedText = {
@@ -73,19 +171,20 @@ const PEnracinee = ({lang, texts}) => {
 			<Banniere>
 				<div className='temp'>
 					<h1>Bannière</h1>
-					<p>image, titre, bouton sortie rapide</p>
+					<p>[ .... À venir: image, titre et bouton «sortie rapide» .... ]</p>
+					<hr/>
+					<p><i>[Animation à coder par Myriam : les chapitres apparaissent un à la fois en fade-in]</i></p>
 				</div>
 			</Banniere>
-			<hr/>
 			
-			<Video>
-				<iframe src="https://player.vimeo.com/video/235215203?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media; web-share" referrerpolicy="strict-origin-when-cross-origin"></iframe>
-				<script src="https://player.vimeo.com/api/player.js"></script>
-			</Video>
-			
-			<Chapitres>
-			
-				<Chapitre><div id='chapitre1' className=''>
+			<Intro>
+				<Video>
+					<div className='set-height'></div>
+					<iframe src="https://player.vimeo.com/video/235215203?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media; web-share" referrerpolicy="strict-origin-when-cross-origin"></iframe>
+					<script src="https://player.vimeo.com/api/player.js"></script>
+				</Video>
+				
+				<Chapitre><div id='chapitre1' className='chapitre'>
 					<StaticImage 
 						src='../images/enracinee/temp-1.png'
 						alt='temp'
@@ -95,8 +194,11 @@ const PEnracinee = ({lang, texts}) => {
 						{texts.bloc1.texte.map((item, index) => <p key={index}>{item}</p>)}
 					</div>
 				</div></Chapitre>
+			</Intro>
+			
+			<Chapitres>
 					
-				<Chapitre><div id='chapitre2' className='color2'>
+				<Chapitre><div id='chapitre2' className='chapitre color2'>
 					<StaticImage 
 						src='../images/enracinee/temp-2.png'
 						alt='temp'
@@ -107,7 +209,7 @@ const PEnracinee = ({lang, texts}) => {
 					</div>
 				</div></Chapitre>
 				
-				<Chapitre><div id='chapitre3' className='color3'>
+				<Chapitre><div id='chapitre3' className='chapitre color3'>
 					<StaticImage 
 						src='../images/enracinee/temp-3.png'
 						alt='temp'
@@ -118,7 +220,7 @@ const PEnracinee = ({lang, texts}) => {
 					</div>
 				</div></Chapitre>
 				
-				<Chapitre><div id='chapitre4' className='color4'>
+				<Chapitre><div id='chapitre4' className='chapitre color4'>
 					<StaticImage 
 						src='../images/enracinee/temp-4.png'
 						alt='temp'
@@ -129,7 +231,7 @@ const PEnracinee = ({lang, texts}) => {
 					</div>
 				</div></Chapitre>
 				
-				<Chapitre><div id='chapitre5' className='color3'>
+				<Chapitre><div id='chapitre5' className='chapitre color3'>
 					<StaticImage 
 						src='../images/enracinee/temp-5.png'
 						alt='temp'
@@ -140,7 +242,7 @@ const PEnracinee = ({lang, texts}) => {
 					</div>
 				</div></Chapitre>
 				
-				<Chapitre><div id='chapitre6' className='color2'>
+				<Chapitre><div id='chapitre6' className='chapitre color2'>
 					<StaticImage 
 						src='../images/enracinee/temp-6.png'
 						alt='temp'
@@ -152,6 +254,13 @@ const PEnracinee = ({lang, texts}) => {
 				</div></Chapitre>
 				
 			</Chapitres>
+			
+			<Banniere>
+				<div className='temp'>
+					<hr/>
+					<p><i>[Appels à l'action : à venir]</i></p>
+				</div>
+			</Banniere>
 		
 		</div>
 	)
