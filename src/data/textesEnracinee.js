4696bc2e930e1609e0172b97585a2b47b099a512
MyriamB on Nouvelle vague <myriam@myriadeweb.com>
2025-11-11 15:16:12 -0500

    Enracinee : Textes Fr finaux hardcodés

--- src/data/textesEnracinee.js
+++ src/data/textesEnracinee.js
@@ -1,54 +1,59 @@
 export default function getTexts() { return {
 	fr: {
 		bloc1: {
-			titre: '[1] Le parrainage',
+			titre: 'Le parrainage',
 			texte: [
-				"Hana, originaire de Beyrouth, rejoint au Québec son mari Maxime, rencontré lors d’un &nbsp;<strong>&nbsp;échange étudiant&nbsp;</strong>&nbsp;à Barcelone.",
-				"Entrée au pays avec un visa de visiteur, elle rêve d’y bâtir une vie et une famille. Ensemble, ils déposent une demande sous le programme de parrainage des époux·se·s afin qu’Hana puisse obtenir la résidence permanente.",
-				"Quelques mois plus tard, Hana met au monde leur premier enfant. Maxime la rassure : il s’occupera des démarches d’immigration, elle n’a pas à s’en soucier ; grâce à lui, elle peut rester au Canada."
+				"Hana, originaire de Beyrouth, quitte son pays pour rejoindre au Québec son mari Maxime, qu’elle a rencontré lors d’un <strong>&nbsp;échange étudiant&nbsp;</strong>&nbsp; à Barcelone.",
+				"Au grand regret de sa famille, elle renonce à la carrière qu’elle avait commencée au Liban pour s’installer au Canada et y construire une vie, puis une famille, avec Maxime.",
+				"Quelques mois après son arrivée, Hana apprend qu’elle est enceinte de leur premier enfant.",
+				"Entrée au pays avec un visa de visiteur, le couple décide de déposer une demande de résidence permanente dans le cadre du programme de parrainage des époux et épouses.",
+				"Maxime la rassure : il s’occupera des démarches d’immigration, elle n’a pas à s’en soucier ; grâce à lui, elle peut rester au Canada."
 			]
 		},
 		bloc2: {
-			titre: '[2] La violence et le contrôle coercitif',
+			titre: 'La violence et le contrôle coercitif',
 			texte: [
 				"Depuis son arrivée à Sherbrooke, Maxime n’est &nbsp;<strong>&nbsp;plus le même.&nbsp;</strong>",
-				"Il exige qu’Hana partage sa géolocalisation avec lui à tout moment, disant que c’est surtout pour s’assurer qu’elle soit en sécurité puisqu’elle est « nouvelle ici » et « ne connaît pas la ville ».",
-				"Après l’avoir convaincu de rester à la maison pendant sa grossesse, il lui reproche sa dépendance envers lui. Il contrôle ses achats, exige ses mots de passe, interdit qu’elle parle à sa famille ou à ses amies sans sa présence. Il la rabaisse sans cesse, la traitant d’« incompétente », de « bonne à rien ».",
-				"Les seules personnes qu’Hana connaît au Québec sont les amis de Maxime. Elle ne sait pas vers qui se tourner, elle craint qu’il surveille ses messages et s’en prenne à elle ou son enfant… Peu à peu, la peur s’installe: elle ne sait plus quoi faire."
+				"Il exige qu’Hana partage sa géolocalisation avec lui à tout moment, disant que c’est surtout pour s’assurer qu’elle soit en sécurité puisqu’elle est « nouvelle ici » et « ne connaît pas la ville ».  Il contrôle ses achats, exige ses mots de passe, interdit qu’elle parle à sa famille ou à ses amies sans sa présence.",
+				"Après l’avoir convaincu de rester à la maison pendant sa grossesse et après l’accouchement, il lui reproche sa dépendance envers lui. Il la rabaisse sans cesse, la traitant d’« incompétente », de « bonne à rien ».",
+				"Les seules personnes qu’Hana connaît au Québec sont les proches de Maxime. Elle ne sait pas vers qui se tourner, elle craint qu’il surveille ses messages et s’en prenne à elle ou son enfant… Peu à peu, la peur s’installe: elle ne sait plus quoi faire."
 			]
 		}, 
 		bloc3: {
-				titre: "[3] Les menaces liées à son statut et les craintes de dénoncer",
+				titre: "Les menaces liées à son statut et les craintes de dénoncer",
 				texte: [
-					"Après une &nbsp;<strong>&nbsp;violente dispute&nbsp;</strong>&nbsp;, Hana éclate, elle n’en peut plus, elle songe à partir.", 
-					"Il hurle, «C’est grâce à moi que tu es ici! Sans moi, tu n’es rien, tu n’as rien. » Il la menace d’appeler l’Agence des services frontaliers du Canada ou la police, l’accusant d’enlèvement si elle quitte avec leur enfant. Un jour, il lui arrache une lettre d’immigration de ses mains et confisque tous ses papiers : ses documents d’immigration, son passeport, son acte de naissance et celui de l’enfant, son certificat de mariage...",
-					"Hana lui faisait confiance. Elle craint qu’en le quittant, sa demande de parrainage soit rejetée. L’angoisse la ronge. Hana pense sans cesse aux conséquences que quitter son mari aurait sur son statut d’immigration."
+					"Un jour, il lui arrache une lettre d’immigration de ses mains et &nbsp;<strong>confisque tous ses papiers&nbsp;</strong>&nbsp;:",
+					"ses documents d’immigration, son passeport, son acte de naissance et celui de l’enfant, son certificat de mariage… Tout ce qui prouve son existence légale au Canada disparaît entre ses mains.",
+					"Hana pense sans cesse aux conséquences qu’aurait une séparation. Elle sait que si elle quitte Maxime, il peut retirer la demande de parrainage, et que celle-ci sera automatiquement rejetée, puisque le programme exige que les époux et épouses vivent ensemble. Elle comprend aussi ce que cela signifie : plus le droit de travailler, plus accès à l’assurance maladie, aucune protection si quelque chose lui arrive.",
+					"Son visa de visiteur étant maintenant expiré, elle se retrouverait sans statut d’immigration. Sans revenu. Sans couverture médicale. Et, peut-être même, déportée du pays.",
+					"Après une violente dispute, Hana éclate, elle n’en peut plus, elle pense sérieusement à partir. Il hurle, « C’est grâce à moi que tu es ici ! Sans moi, tu n’es rien, tu n’as rien ». Il menace de retirer la demande de parrainage et d’appeler les autorités d’immigration. Il menace même d’appeler la police et de l’accuser d’enlèvement si elle part avec leur enfant."
 				]
 			},
 		bloc4: {
-			titre: "[4] Les obstacles à l’accès à la justice",
+			titre: "Les obstacles à l’accès à la justice",
 			texte: [
 				"Hana trouve le &nbsp;<strong>&nbsp;courage&nbsp;</strong>&nbsp; d’appeler une ressource d’aide.", 
-				"Soutenue par une intervenante, elle dépose une plainte à la police.",
+				"Soutenue par une intervenante communautaire, elle dépose une plainte à la police.",
 				"Très vite, les démarches se multiplient : un avocat en droit de la famille pour la garde de l’enfant, une avocate pour la plainte criminelle, une autre en immigration pour régulariser son statut et obtenir un permis de travail. Des étapes indispensables pour espérer reconstruire sa vie.",
+				"Sachant que sa demande de parrainage risque d’être rejetée, Hana doit rapidement trouver une solution pour régulariser son statut et, éventuellement, obtenir la résidence permanente.",
 				"Sans revenu, ni soutien de ses proches au Liban, Hana n’a pas les moyens de payer les frais d'avocats au privé. Elle se sent dépourvue.",
 				"Sachant que sa demande de parrainage risque d’être rejetée, Hana doit rapidement trouver une solution pour régulariser son statut et, éventuellement, obtenir la résidence permanente."
 			]
 		},
 		bloc5: {
-			titre: "[5] Titre à déterminer",
+			titre: "Un accompagnement socio-juridique",
 			texte: [
-				"Une consultation gratuite lui est accordée grâce à l’&nbsp;<strong>&nbsp;aide juridique.&nbsp;</strong>",
+				"Finalement, une consultation gratuite lui est accordée grâce à l’&nbsp;<strong>&nbsp;aide juridique.&nbsp;</strong>",
 				"On lui explique que la voie la plus sûre serait de demander un Permis de séjour temporaire pour victimes de violence familiale. Mais faute de ressources, personne de l’aide juridique ne pourra la représenter pour cette démarche.",
-				"Hana appelle sans relâche les avocats et avocates inscrites à l’aide juridique. Chaque numéro mène au même constat : des avocats débordés, incapables de prendre son dossier. Elle raccroche, encore et encore, avec le sentiment de frapper à des portes closes.",
+				"Hana appelle sans relâche les avocats et avocates  qui prennent des dossiers sous l’aide juridique. Chaque numéro mène au même constat : des avocats débordés, incapables de prendre son dossier. Elle raccroche, encore et encore, avec le sentiment de frapper à des portes closes.",
 				"Avec le soutien d’une travailleuse sociale, Hana trouve refuge en maison d’hébergement pour elle et son enfant. On la réfère à une clinique juridique spécialisée en droit de l’immigration humanitaire. Grâce à leur soutien, elle obtient un Permis de séjour temporaire d’un an. Mais il lui faudra bientôt trouver une autre façon de rester au pays."
 			]
 		},
 		bloc6: {
-			titre: "[6] Un chemin périlleux",
+			titre: "Un avenir périlleux",
 			texte: [
-				"Les mois passent, son permis approche à l’expiration, tout comme la date du &nbsp;<strong>&nbsp;procès au criminel.&nbsp;</strong>", 
-				"Hana ignore si elle pourra témoigner contre Maxime si sa demande de renouvellement est refusée, ce qui est probable considérant que le taux de renouvellement demeure faible.",
+				"Les mois passent, son permis arrive à expiration et la date du &nbsp;<strong>&nbsp;procès au criminel approche&nbsp;</strong>.",
+				"Hana ignore si elle pourra témoigner contre Maxime si la demande de renouvellement de son Permis de séjour temporaire est refusée, ce qui est probable considérant que le taux de renouvellement demeure faible.",
 				"Elle redoute de devoir retourner au Liban, où les liens familiaux se sont brisés depuis son départ. Ses parents et ses sœurs n’ont jamais accepté qu’elle abandonne sa carrière en quittant le Liban. Coupée d’eux depuis qu’elle a déménagé au Québec, Hana porte la honte et le regret d’avoir laissé le silence s’installer. Elle craint désormais d’être rejetée une seconde fois, avec son enfant canadien.",
 				"Sans proches ni amis au Québec, l’avenir d’Hana demeure en suspens, fragile, incertain."
 			]
