f38e74279c9ac2b1a8613ddc5168b8b6fe8799a1
MyriamB on Nouvelle vague <myriam@myriadeweb.com>
2026-03-10 09:08:49 -0400

    SEO SMO corrections

--- src/pages/enraciner.en.js
+++ src/pages/enraciner.en.js
@@ -19,12 +19,12 @@ export default Page
 export const Head = () => (
   <>
     <html lang='fr' />
-    <title>Enraciner | Solutions justes</title>
+    <title>Enraciner : Trapped in Migratory Vines | Solutions justes</title>
     <meta name='description' content='When a person’s immigration status depends on a violent partner, neither staying nor leaving can guarantee their safety. Discover Hana’s story and why urgent action is needed.' />
     
-    <meta property='og:title' content='Enraciner | Solutions justes' />
+    <meta property='og:title' content='Enraciner : Trapped in Migratory Vines | Solutions justes' />
     <meta property='og:description' content='When a person’s immigration status depends on a violent partner, neither staying nor leaving can guarantee their safety. Discover Hana’s story and why urgent action is needed.' />
-    <meta property='og:image' content='https://rever.solutionsjustes.org/images/enraciner/enraciner-solutions-justes-og' />
+    <meta property='og:image' content='https://rever.solutionsjustes.org/images/enraciner/enraciner-solutions-justes-og.png' />
     <meta property='og:image:alt' content='Enraciner' />
     <meta property='og:url' content='https://rever.solutionsjustes.org/en/enraciner/' />
     <meta property='og:type' content='website' />
