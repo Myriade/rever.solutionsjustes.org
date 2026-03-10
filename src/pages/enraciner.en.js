27e334ffc03ce9fa36e8d1e23878d034dce09fde
MyriamB on Nouvelle vague <myriam@myriadeweb.com>
2026-03-10 12:23:15 -0400

    Enraciner : meta tags OG images sans argument de version

--- src/pages/enraciner.en.js
+++ src/pages/enraciner.en.js
@@ -24,12 +24,12 @@ export const Head = () => (
     
     <meta property='og:title' content='Enraciner : Trapped in Migratory Vines | Solutions justes' />
     <meta property='og:description' content='When a person’s immigration status depends on a violent partner, neither staying nor leaving can guarantee their safety. Discover Hana’s story and why urgent action is needed.' />
-    <meta property='og:image' content='https://rever.solutionsjustes.org/images/enraciner/enraciner-solutions-justes-og-2?v=3.png' />
+    <meta property='og:image' content='https://rever.solutionsjustes.org/images/enraciner/enraciner-solutions-justes-og-2.png' />
     <meta property='og:image:alt' content='Enraciner' />
     <meta property="og:image:width" content="1200" />
     <meta property="og:image:height" content="630" />
     <meta name="twitter:card" content="summary_large_image" />
-    <meta name="twitter:image" content="https://rever.solutionsjustes.org/images/enraciner/enraciner-solutions-justes-og-2.png?v=3" />
+    <meta name="twitter:image" content="https://rever.solutionsjustes.org/images/enraciner/enraciner-solutions-justes-og-2.png" />
     <meta property='og:url' content='https://rever.solutionsjustes.org/en/enraciner/' />
     <meta property='og:type' content='website' />
   </>
--- src/pages/enraciner.fr.js
+++ src/pages/enraciner.fr.js
@@ -24,12 +24,12 @@ export const Head = () => (
     
     <meta property='og:title' content='Enraciner : Prise entre les lierres migratoires | Solutions justes' />
     <meta property='og:description' content='Quand le statut d’immigration d’une personne dépend d’un·e partenaire violent·e, ni rester ni partir ne garantit sa sécurité. Découvrez l’histoire d’Hana et pourquoi il est urgent d’agir' />
-    <meta property='og:image' content='https://rever.solutionsjustes.org/images/enraciner/enraciner-solutions-justes-og-2.png?v=3' />
+    <meta property='og:image' content='https://rever.solutionsjustes.org/images/enraciner/enraciner-solutions-justes-og-2.png' />
     <meta property='og:image:alt' content='Enraciner' />
     <meta property="og:image:width" content="3750" />
     <meta property="og:image:height" content="1969" />
     <meta name="twitter:card" content="summary_large_image" />
-    <meta name="twitter:image" content="https://rever.solutionsjustes.org/images/enraciner/enraciner-solutions-justes-og-2.png?v=3" />
+    <meta name="twitter:image" content="https://rever.solutionsjustes.org/images/enraciner/enraciner-solutions-justes-og-2.png" />
     <meta property='og:url' content='https://rever.solutionsjustes.org/enraciner/' />
     <meta property='og:type' content='website' />
   </>
